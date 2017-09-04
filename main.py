903dfa3928a2d384d968714652c49e779c453e08
Matthew Mi <mzalive@gmail.com>
2017-09-05 06:03:40 +1000

    levenshtein

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
 import fuzzy
+import Levenshtein
 import ngram
 import time
+import json
+import os
 
 token_path = 'data/labelled-tokens.txt'
 dict_path = 'data/dict.txt'
 
 def getDict():
-    dictFile = open(dict_path, 'r')
-    dictSet = [item.strip() for item in dictFile]
-    return  dictSet
+    with open(dict_path, 'r') as dictFile:
+        dictSet = [item.strip() for item in dictFile]
+        return  dictSet
+
 
 def match_levenshtein(token):
-    return
+    dictSet = getDict()
+    candidates = []
+    candidatesG = []
+    bestMatch = ""
+    minDistance = 1
+
+    for item in dictSet:
+        distance = Levenshtein.distance(token.lower(), item.lower())
+        if distance == 0:
+            return item, [], []
+        elif distance < minDistance:
+            minDistance = distance
+            candidates = []
+        if distance == minDistance:
+            candidates.append(item.lower())
+
+    if len(candidates) > 1:
+        G = ngram.NGram(candidates)
+        candidatesG = G.search(token)
+        bestMatch = candidatesG[0][0]
+    elif len(candidates) == 1:
+        bestMatch = candidates[0]
+
+    return bestMatch, candidates, candidatesG
+
 
 def match_soundex(token):
     return
 
+
 def match_double_metaphone(token):
     return
 
-def execute(method):
-    tokenFile = open('data/labelled-tokens.txt', 'r')
 
+def execute(method):
+    results = []
+    count = 0
     if method == 0:
         methodName = 'levenshtein'
         target_method = match_levenshtein
@@ -34,20 +64,51 @@ def execute(method):
 
     timestamp_start = time.time()
 
-    for tokenLine in tokenFile:
-        tokenSet = tokenLine.split('\t')
-        token = tokenSet[0].strip()
-        code = tokenSet[1].strip()
-        canonical = tokenSet[2].strip()
+    with open('data/labelled-tokens.txt', 'r') as tokenFile:
+        for tokenLine in tokenFile:
+            count += 1
+            tokenSet = tokenLine.split('\t')
+            token = tokenSet[0].strip()
+            code = tokenSet[1].strip()
+            canonical = tokenSet[2].strip()
 
-        # best_match, candidates = target_method(token)
+            best_match, candidates, candidatesG = target_method(token)
 
-        # handle result
+            result = {
+                'token': token,
+                'best_match': best_match,
+                'candidates': candidates,
+                'candidatesG': candidatesG,
+                'canonical': canonical
+            }
+
+            results.append(result)
+            print result
+
+            if count == 10:
+                break
 
     timestamp_end = time.time()
     time_elapse = timestamp_end - timestamp_start
+    m, s = divmod(time_elapse, 60)
+
+    output = {
+        'benchmark': {
+            'timestamp_start': time.ctime(timestamp_start),
+            'timestamp_end': time.ctime(timestamp_end),
+            'time_elapse': time_elapse,
+            'time_elapse_readable': '%02d min %02d sec' % (m, s)
+        },
+        'results': results
+    }
+
+    if not os.path.isdir('output'):
+        os.mkdir('output')
+
+    with open('output/' + methodName + '.json', 'w') as fout:
+        json.dump(output, fout)
 
 
-
+execute(0)
 
 
